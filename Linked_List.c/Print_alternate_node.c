34ca1ab601e5f5bf000ad13a830e2421618d3536
Maria <m76131543@gmail.com>
2026-07-25 23:55:02 +0530

    Correct node structure and alternate node function
    Fix struct definition and alternate node printing logic.

--- Linked_List.c/Print_alternate_node.c
+++ Linked_List.c/Print_alternate_node.c
@@ -1,29 +1,35 @@
-#inlcude<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 struct Node {
   int data;
-  struct *next;
+  struct Node *next;
 };
 
 void alternate_node(struct Node*head) {
   int i=1;
- while ( head!=NULL) {
+  while ( head!=NULL) {
+
       if( i%2==1) 
-printf("%d", head->data);
-c++;
-head= head->next;
-return 0;
- }
+       printf(" %d ", head->data);
+       i++;
+       head= head->next;
+
+    }
+}
 
 int main()
 {
     struct Node *head = NULL;
     struct Node *second = NULL;
     struct Node *third = NULL;
-
+    struct Node *forth= NULL;
+    struct Node *fifth= NULL;
+     
     head = (struct Node *)malloc(sizeof(struct Node));
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
+    forth=(struct Node *)malloc(sizeof(struct Node));
+    fifth=(struct Node *)malloc(sizeof(struct Node));
 
     head->data = 10;
     head->next = second;
@@ -32,7 +38,13 @@ int main()
     second->next = third;
 
     third->data = 30;
-    third->next = NULL;
+    third->next = forth;
+    
+    forth->data= 40;
+    forth->next=fifth;
+    
+    fifth->data=50;
+    fifth->next=NULL;
 
  alternate_node(head);
 return 0;
