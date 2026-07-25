296657d89a4a9a2d9aa0af09b25a5321b7c2eab2
Maria <m76131543@gmail.com>
2026-07-25 23:32:37 +0530

    Implement main function for linked list example
    Added main function to create a linked list and call alternate_node function.

--- Linked_List.c/Print_alternate_node.c
+++ Linked_List.c/Print_alternate_node.c
@@ -14,3 +14,26 @@ c++;
 head= head->next;
 return 0;
  }
+
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+
+ alternate_node(head);
+return 0;
+}
