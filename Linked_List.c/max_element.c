8a0bf2769952bad1f33b42f3e2b320da21fb50d2
Maria <m76131543@gmail.com>
2026-09-04 12:47:08 +0530

    Implement max_element function for linked list

---
+++ Linked_List.c/max_element.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <limits.h>
+
+struct Node{
+  int data;
+  struct Node* next;
+  
+};
+
+struct Node* createNode(int val) {
+  struct Node* newNode= malloc(sizeof(struct Node));
+  newNode->data=val;
+  newNode->next=NULL;
+  return newNode;
+}
+int maximumElement(struct Node* head) {
+  int max=INT_MIN;
+  struct Node* temp=head;
+  while(temp!=NULL) {
+    if(temp->data>max) {
+      max=temp->data;
+    }
+    temp=temp->next;
+  }
+  return max;
+
+void display (struct Node* head){
+  struct Node* temp=head;
+
+  while(temp!=NULL) {
+    printf("%d->", temp->data);
+    temp=temp->next;
+    
+  }
+  printf("NULL\n");
+}
+  int main() {
+    struct Node* head=createNode(10);
+    head->next=createNode(20);
+    head->next->next=createNode(30);
+    display(head);
+printf(" maximum element= %d\n", maximumElement(head));
+    return 0;
+    
+}
+
