66a58811d19043f211089678c5b3fa2f333b579c
Maria <m76131543@gmail.com>
2026-09-17 21:59:35 +0530

    Implement display function for stack
    Added display function to show stack contents.

--- Stack/Pop_stack.c
+++ Stack/Pop_stack.c
@@ -14,3 +14,22 @@ int pop() {
     top--;
     return val;
 }
+void display() {
+    if(top==-1) {
+        printf("stack is empty");
+        return ;
+   }
+    for(int i=top; i>=0; i--) {
+        printf(" %d", arr[i]);
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    push(50);
+printf("\npopped elemnt= %d", pop());
+    display();
+}
