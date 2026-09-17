612b37121bbe3b0cb4cc22817b5fe0b4f57a29b8
Maria <m76131543@gmail.com>
2026-09-17 22:00:32 +0530

    Implement display function for stack
    Added display function to show stack contents.

--- Stack/Push_stack.c
+++ Stack/Push_stack.c
@@ -13,3 +13,21 @@ void push(int val) {
     top++;
     arr[top]=val;
 }
+
+void display() {
+    if(top==-1) {
+        printf("stack is empty");
+        return ;
+   }
+    for(int i=top; i>=0; i--) {
+        printf(" %d", arr[i]);
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    push(50);
+    display();
